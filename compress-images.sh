750e22a4008489a530ff0ac5c0db05bef1d67be7
Doug Bell <madcityzen@gmail.com>
2015-10-29 23:19:24 -0500

    fix images being given the wrong extension
    The compress script would turn "IMG.jpg" to "IMG.jpg.big", which OS X
    doesn't really like (nor, I suspect, would any other OS).

--- compress-images.sh
+++ compress-images.sh
@@ -3,6 +3,7 @@ ARGS='-resample 72 -resize 35% -quality 65%'
 
 for IMG in $@; do
     echo $IMG
-    mv $IMG $IMG.big
-    convert $IMG.big $ARGS $IMG
+    NEWNAME=$(echo $IMG | sed 's/.jpg$/.big.jpg/')
+    mv $IMG $NEWNAME
+    convert $NEWNAME $ARGS $IMG
 done
